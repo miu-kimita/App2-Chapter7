d03eabe99dbc652ef786c7806050d993fbe5e3ee
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-04-17 05:17:33 +0000

    [Finish]5-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
+
+Refile.secret_key = '45dc193b74bf5f99e6fa719576c0fb63ae526f3e6c04dd16f0fe6caa65d4a9b43b2853d58da926a72041970679d61f8e8d3212834bfb98386e8a2828fd6dd92c'
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '45dc193b74bf5f99e6fa719576c0fb63ae526f3e6c04dd16f0fe6caa65d4a9b43b2853d58da926a72041970679d61f8e8d3212834bfb98386e8a2828fd6dd92c'
